e7c97547a82c3f63285fb26b4638c35889546ff2
Ivanvegiani <ivanvegiani@gmail.com>
2018-02-13 09:44:41 -0200

    inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from xml_gpx import Xml_gpx
+
+
+#iniciando automaticamente
+i1=Xml_gpx()
+i1.in_model()
---
+++ xml_gpx.py
@@ -0,0 +1,50 @@
+#coding: utf-8
+from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree as ET
+
+
+'''
+<?xml version='1.0' encoding='UTF-8' standalone='yes' ?> # declaração XMl
+<gpx version="1.1" creator="COPEL" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"> #root e seus atributos
+  <wpt lat="-26.1860003" lon="-50.9284544"> elemento  tag "wpt" atributos lat="-26.1860003" lon="-50.9284544"
+    <name>876759780</name> tag nane e text 876759780
+    <category>Geral</category>tag category e text Geral
+  </wpt>
+</gpx>
+'''
+
+
+
+class Xml_gpx(ElementTree):
+
+
+    def in_model(self):
+        tree = ET.parse('input_model.xml')
+        ET.register_namespace('','http://www.topografix.com/GPX/1/1')
+        ET.register_namespace("xsi",'http://www.w3.org/2001/XMLSchema-instance')
+        tree.write('favourites.gpx', encoding='UTF-8')
+        ET.dump(tree)
+ET.register_namespace('','http://www.topografix.com/GPX/1/1')
+ET.register_namespace("xsi",'http://www.w3.org/2001/XMLSchema-instance')
+tree = ET.parse('input_model.xml') #importa o xml e atribiu na árvore
+root = tree.getroot() # captura a tag gpx como o root
+lat='lat'
+lon='lon'
+wpt_atributos={'lat':lat,'lon':lon}
+wpt=ET.Element('wpt',wpt_atributos)
+name=ET.SubElement(wpt, 'name')
+category=ET.SubElement(wpt, 'category')
+root.append(wpt)
+
+ET.dump(root)
+
+
+#for elemento in tree.iter():
+#    print(elemento.tag)
+#    print(elemento.attrib)
+#print(ET.iselement(wpt))
+
+
+
+#tree.write('favourites.gpx', encoding='UTF-8')
