51ad8e061ece974f8e417a0a95141d390a4ccc10
Ivanvegiani <ivanvegiani@gmail.com>
2018-02-19 23:34:40 -0300

    quase lá

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ from import_csv import ImportCsv
 # xml_gps é um módulo utilizado para testes
 
 path_input='/media/ivanvegiani/repositorios/Projetos/ConvertGpx/input/modelo.csv'
+path_output='/media/ivanvegiani/repositorios/Projetos/ConvertGpx/output/favourites.gpx'
 
 #----------------------testes unitários ----------------------
 
 # instancias de testes t1 e t2
-t1=ImportCsv(path_input)
+# t1=ImportCsv(path_input)
 # t2=XmlGpx()
 # t1.reseta_atributos()
 # t2.set_wpt_atributos('-260000','-266655',0)
@@ -48,14 +49,13 @@ t1=ImportCsv(path_input)
 # ------------------ main---------------------------------------
 # importando o CSV e atrinbuindo os atributos lat, lon, name e category
 #intancia 1 ImportCsv
-#intancia 2 xml_gpx
-i11=ImportCsv(path_input)
+i1=ImportCsv(path_input)
 names_csv=[]
 categorys_csv=[]
 lats_lons_csv=[]
-names_csv=i11.get_names_csv()
-categorys_csv=i11.get_categorys_csv()
-lats_lons_csv=i11.get_lats_lons_csv()
+names_csv=i1.get_names_csv()
+categorys_csv=i1.get_categorys_csv()
+lats_lons_csv=i1.get_lats_lons_csv()
 # print(names_csv)
 # print(categorys_csv)
 # print(lats_lons_csv)
@@ -64,10 +64,25 @@ lats_lons_csv=i11.get_lats_lons_csv()
 ET.register_namespace('','http://www.topografix.com/GPX/1/1')
 ET.register_namespace("xsi",'http://www.w3.org/2001/XMLSchema-instance')
 tree = ET.parse('input_model.xml') #importa o xml e atribiu na árvore
-root = tree.getroot() # captura a tag gpx como o root
-# for rows in categorys_csv:
-#     wpt
-
-
-
+root = tree.getroot()
+# wpt_atributos={}
+# wpt=ET.Element('wpt')
+# name=ET.SubElement(wpt, 'name')
+# category=ET.SubElement(wpt, 'category')
+# wpt.attrib={'lat':'-fdfdf','lon':'fodkofdk'}  <== exemplo para atributos lat lon
+# name.text='nome' <== exemplo para texto
+# category.text='categoria'<== exemplo para texto
+# root.append(wpt)  <== adiciona o elemento wpt para árvore
+# print(lats_lons_csv[5])
+i=0
+name=['']
+for rows in lats_lons_csv:
+    wpt=ET.Element('wpt',lats_lons_csv[i])
+    name=ET.SubElement(wpt, 'name')
+    name.text=names_csv[i]
+    category=ET.SubElement(wpt, 'category')
+    category.text=categorys_csv[i]
+    root.append(wpt)
+    i=i+1
 ET.dump(tree)
+tree.write(path_output,encoding='UTF-8')
